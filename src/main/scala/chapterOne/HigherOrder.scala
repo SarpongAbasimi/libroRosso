ddcf78fe62392965098d9b45681c7bf1682f2c3d
sarpong <abasimisarpong@rocketmail.com>
2021-07-24 12:21:25 +0100

    curry

--- src/main/scala/chapterOne/HigherOrder.scala
+++ src/main/scala/chapterOne/HigherOrder.scala
@@ -9,6 +9,11 @@ object HigherOrder {
   def higherFunction(anInt:Int, f: (Int) =>  Int):Int = {
     f(anInt)
   }
+
+  def curry[A, B, C](f: (A, B) => C): (A) => (B => C) = {
+    a => b => f(a, b)
+  }
+
   def main(args: Array[String]): Unit = {
     val returnedValue = higherFunction(4, someMethod)
     println(returnedValue)
